c30af461052ea01919c68eea6cfee2e54b678944
村上幸雄 <yukio@mbpci7.local>
2015-09-27 23:04:27 +0900

    first commit

---
+++ lisp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lisp
+//
+//  Created by 村上幸雄 on 2015/09/27.
+//  Copyright © 2015年 MURAKAMI Yukio. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
