b273fcd406b814ddac77a1c0e6782c0561c95542
murakami <m_yukio@mac.com>
2023-05-04 11:59:48 +0900

    REPL

--- lisp/main.swift
+++ lisp/main.swift
@@ -14,9 +14,11 @@ class SExp {
 }
 
 class Nihil: SExp {
+    static let sharedInstance = Nihil()
 }
 
 class True: SExp {
+    static let sharedInstance = Nihil()
 }
 
 class Atom: SExp {
@@ -32,7 +34,39 @@ class Symbol: Atom {
 }
 
 class ConsCell: SExp {
+    var car: SExp
+    var cdr: SExp
+    
+    override init() {
+        car = Nihil.sharedInstance
+        cdr = Nihil.sharedInstance
+    }
+    
+    init(car: SExp, cdr: SExp) {
+        self.car = car
+        self.cdr = cdr
+    }
 }
 
 class Environment {
+    let seNihil = Nihil.sharedInstance
+    let seTrue = True.sharedInstance
+    
+    func car(e: ConsCell) -> SExp {
+        return e.car
+    }
+    
+    func cdr(e: ConsCell) -> SExp {
+        return e.cdr
+    }
 }
+
+while true {
+    print("> ", terminator:"")
+    guard let line = readLine() else { break }
+    if line == "quit" { break }
+    print("\(line)")
+}
+print("quit")
+
+/* End Of File */
